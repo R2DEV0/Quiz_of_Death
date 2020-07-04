6322283a87df16441d6678b91940016abf936211
R2DEV0 <r2devo@gmail.com>
2020-07-04 12:16:31 -0700

    updates

---
+++ client/build/precache-manifest.0968e7a7d84f2d48e287e6b4c9ff41b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ac59ca06a891d4ce1bbbd0cda5e917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a698bdcc8100f4213022",
+    "url": "/static/css/2.4e281748.chunk.css"
+  },
+  {
+    "revision": "6717be42b66fbe6d5abe",
+    "url": "/static/css/main.6e538467.chunk.css"
+  },
+  {
+    "revision": "a698bdcc8100f4213022",
+    "url": "/static/js/2.39ce2d3a.chunk.js"
+  },
+  {
+    "revision": "c5b3b5f5df5cbfb735d3e9f8565c687f",
+    "url": "/static/js/2.39ce2d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6717be42b66fbe6d5abe",
+    "url": "/static/js/main.21a6ba2f.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "f24fdc32fbc9f69303aa59f6b8f62791",
+    "url": "/static/media/skull.f24fdc32.gif"
+  }
+]);
